381d1e04df215aef61184922eb93418d04c0ab2e
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-07-04 01:25:35 -0700

    Create class component

---
+++ src/pages/counter.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// class component
+class Counter extends React.Component {
+    render() {
+        return <div>Hello Class Component</div>;
+    }
+}
+
+export default Counter;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "gatsby-link";
 
+// functional component
 export default () => (
     <div>
         <p>Hello world from my second Gatsby page</p>
