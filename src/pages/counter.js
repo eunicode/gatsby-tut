9a7401c78f3d7611457396c5c3f56e1aae0b93d4
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-07-04 01:40:42 -0700

    Create counter, display/update state

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -2,9 +2,28 @@ import React from "react";
 
 // class component
 class Counter extends React.Component {
-    render() {
-        return <div>Hello Class Component</div>;
-    }
+  // Set up component state
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        {/* Render current count from component state */}
+        <p>current count: {this.state.count}</p>
+        {/* Change the state when we click on buttons */}
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          plus
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+          minus
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
